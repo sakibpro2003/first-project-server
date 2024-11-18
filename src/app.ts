6ff0cbb461445131df39e29be10a5b24d6b40805
sakibpro2003 <sakibprodhan2003@gmail.com>
2024-11-18 22:40:11 +0600

    get single data of student

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,21 @@
-import express, { Application, Request, Response } from 'express'
-import cors from 'cors'   
-const app: Application= express()
-const port = 3000
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+import { StudentRoutes } from './app/modules/student/student.route';
+
+const app: Application = express();
 
 //parsers
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
-})
+// application routes
+app.use('/api/v1/students', StudentRoutes);
+
+const getAController = (req: Request, res: Response) => {
+  // const a = 10;
+  res.send('runing');
+};
+
+app.get('/', getAController);
 
 export default app;
-
-console.log(process.cwd())
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,19 @@
-import app from "./app";
-import config from "./app/config";
+import app from './app';
+import config from './app/config';
+import mongoose from 'mongoose';
 
-import mongoose from "mongoose";
-
-const sd = 34;
+// const sd = 34;
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
 
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
+      console.log(` app listening on port ${config.port}`);
     });
   } catch (err) {
     console.log(err);
   }
 }
 
-main()
+main();
