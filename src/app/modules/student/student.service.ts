1e2e447c73f47c80d9a8c4f2398c9494528f504f
sakibpro2003 <sakibprodhan2003@gmail.com>
2024-12-09 21:46:26 +0600

    field filtering added

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -19,7 +19,7 @@ const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
   });
 
   //filtering
-  const excludeFields = ['searchTerm', 'sort', 'limit'];
+  const excludeFields = ['searchTerm', 'sort', 'limit','page','fields'];
   excludeFields.forEach((el) => delete queryObj[el]);
   const filterQuery = searchQuery
     .find(queryObj)
@@ -38,14 +38,31 @@ const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
 
   const sortQuery = filterQuery.sort(sort);
 
+  let page = 1;
   let limit = 1;
+  let skip = 0;
+
   if (query.limit) {
-    limit = query.limit as number;
+    limit = Number(query.limit);
   }
 
-  const limitQuery = sortQuery.limit(limit);
+  if(query.page){
+    page = Number(query.page);
+    skip = (page-1)*limit
+  }
 
-  return limitQuery;
+
+
+  const paginateQuery = sortQuery.skip(skip);
+  const limitQuery = paginateQuery.limit(limit);
+  let fields = '-__v' //exclude __v 
+  if(query.fields){
+    fields = (query.fields as string).split(',').join(" ");
+  }
+
+  const fieldQuery = await limitQuery.select(fields);
+
+  return fieldQuery;
 };
 
 const getSingleStudentFromDB = async (id: string) => {
